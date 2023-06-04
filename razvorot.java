19b2cb92999352d865a3ff0ef895d8c744fb6f12
valentsk <skredovavalenti@mail.ru>
2023-06-04 15:00:18 +0300

    Разворот двусвязного списка

--- razvorot.java
+++ razvorot.java
@@ -1,5 +1,29 @@
 package homework.algo;
 
-public class razvorot {
-    
+public class list {
+    Node head;
+    Node tail;
+
+    public void revert(){
+        Node currentNode = head;
+        while (currentNode != null) {
+            Node next = currentNode.next;
+            Node previous = currentNode.previous;
+            currentNode.next = previous;
+            currentNode.previous = next;
+            if (previous == null) {
+                tail = currentNode;
+            }
+            if (next == null) {
+                head = currentNode;
+            }
+            currentNode = next;
+        }
+    }
+
+    public class Node{
+        int value;
+        Node next;
+        Node previous;
+    }
 }
